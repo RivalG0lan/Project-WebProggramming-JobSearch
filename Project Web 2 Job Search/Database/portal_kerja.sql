4a7b2a35010ce19c7380a5615a8e66bb5cbd44b3
Maulia Revani Putri <mauliaarvn@gmail.com>
2026-05-14 15:20:23 +0700

    update progres back end

---
+++ Project Web 2 Job Search/Database/portal_kerja.sql
@@ -0,0 +1,133 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 14, 2026 at 09:59 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `portal_kerja`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lamaran`
+--
+
+CREATE TABLE `lamaran` (
+  `id_lamaran` int(11) NOT NULL,
+  `id_lowongan` int(11) NOT NULL,
+  `id_pelamar` int(11) NOT NULL,
+  `status` varchar(50) NOT NULL DEFAULT 'dikirim',
+  `tanggal_lamaran` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lowongan`
+--
+
+CREATE TABLE `lowongan` (
+  `id_lowongan` int(11) NOT NULL,
+  `id_perusahaan` int(11) DEFAULT NULL,
+  `judul` varchar(255) DEFAULT NULL,
+  `kategori` varchar(100) DEFAULT NULL,
+  `lokasi` varchar(100) DEFAULT NULL,
+  `gaji` varchar(100) DEFAULT NULL,
+  `deskripsi` text DEFAULT NULL,
+  `tanggal_post` timestamp NOT NULL DEFAULT current_timestamp(),
+  `status` varchar(20) NOT NULL DEFAULT 'aktif'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `lowongan`
+--
+
+INSERT INTO `lowongan` (`id_lowongan`, `id_perusahaan`, `judul`, `kategori`, `lokasi`, `gaji`, `deskripsi`, `tanggal_post`, `status`) VALUES
+(2, 2, 'UI/UX ', 'Design', 'Perbaungan', '2000000', 'BAntu bantu bos', '2026-05-13 05:08:41', 'aktif');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id_user` int(11) NOT NULL,
+  `nama` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `role` enum('pelamar','perusahaan') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id_user`, `nama`, `email`, `password`, `role`) VALUES
+(1, 'Dinda Arumanis', 'dnda@gmail.com', '670b14728ad9902aecba32e22fa4f6bd', 'pelamar'),
+(2, 'PT Mentega Rvn', 'rvnn@gmail.com', '670b14728ad9902aecba32e22fa4f6bd', 'perusahaan');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `lamaran`
+--
+ALTER TABLE `lamaran`
+  ADD PRIMARY KEY (`id_lamaran`);
+
+--
+-- Indexes for table `lowongan`
+--
+ALTER TABLE `lowongan`
+  ADD PRIMARY KEY (`id_lowongan`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `lamaran`
+--
+ALTER TABLE `lamaran`
+  MODIFY `id_lamaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `lowongan`
+--
+ALTER TABLE `lowongan`
+  MODIFY `id_lowongan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
